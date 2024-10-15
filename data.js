3a5d548b1d3138dc5c0d40b1e30344a536b94baf
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-10-15 11:52:40 +0530

    new problem added

--- data.js
+++ data.js
@@ -488,4 +488,36 @@ export const data = [
     return res
     `,
   },
+  {
+    problemNo: 145,
+    heading: "145. Binary Tree Postorder Traversal",
+    image: "./images/145.png",
+    code: `
+    res = []
+
+    def dfs(node):
+      if not node:
+        return
+      dfs(node.left)
+      dfs(node.right)
+      res.append(node.val)
+
+    dfs(root)
+    return res
+    `,
+  },
+  {
+    problemNo: 160,
+    heading: "160. Intersection of Two Linked Lists",
+    image: "./images/160.png",
+    code: `
+    l1, l2 = headA, headB
+
+    while l1!=l2:
+      l1 = l1.next if l1 else headB
+      l2 = l2.next if l2 else headA
+
+    return l1
+    `,
+  },
 ];
